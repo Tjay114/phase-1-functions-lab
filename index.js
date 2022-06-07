5c3917e4b46a9dafd08408fd917e0b5b0f1ac851
Terormumuli <terahmumuli@gmail.com>
2022-06-07 20:28:27 +0300

    Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(somevalue){
+return  Math.abs (somevalue-42);
+}
+
+function distanceFromHqInFeet(somevalue){
+
+    return distanceFromHqInBlocks(somevalue)*264
+}
+function distanceTravelledInFeet(destination,start){
+    const blocks = destination - start;
+    return Math.abs(blocks * 264)
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance<=400);
+}
